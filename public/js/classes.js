fd1bee50154148975b1d20c9d100a0c616c4330c
Kamil Baldyga <baldygakamil@gmail.com>
2025-04-19 15:58:33 +0200

    add greater hunger substraction for bober pet

--- public/js/classes.js
+++ public/js/classes.js
@@ -175,6 +175,6 @@ export class Dragon extends CyberPet {
 |                                                                        |
 |========================================================================|
 */
-export const bober = new Bober("Bober", "Bobr"); // TODO: add faim diminue plus vite
+export const bober = new Bober("Bober", "Bobr");
 export const cat = new Cat("Kitty", "Cat"); // TODO: add bonheur baisse s'il joue pas souvent
 export const dragon = new Dragon("Draco", "Dragon"); // TODO: add faim diminue très lentement
--- public/js/functions.js
+++ public/js/functions.js
@@ -27,13 +27,19 @@ export function performActionEachInterval(interval = 0, pet) {
 export function statsUsage(pet) {
   const stats = pet.stats[0];
 
-  stats.hunger -= 7;
-  stats.happiness -= 3;
-  stats.energy -= 5;
-
   console.log(`Hunger: ${pet.stats[0].hunger}`);
   console.log(`Happiness: ${pet.stats[0].happiness}`);
   console.log(`Energy: ${pet.stats[0].energy}`);
+
+  // Substract stats each 3000ms
+  // If pet is bober, then hunger stat subsraction is greater than other pets
+  if (pet == bober) {
+    stats.hunger -= 14;
+  } else {
+    stats.huner -= 7;
+    stats.happiness -= 3;
+    stats.energy -= 5;
+  }
 
   // Check if any stats drop to 0
   if (stats.hunger <= 0 || stats.happiness <= 0 || stats.energy <= 0) {
